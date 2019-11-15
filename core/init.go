814d702e6e3142dd5663aece8e7f25355a05198b
HZH <capric98@outlook.com>
2019-11-16 00:56:26 +0800

    refine

--- core/init.go
+++ core/init.go
@@ -126,7 +126,7 @@ func (bot *tgbot) Run() {
 		return
 	}
 
-	srv.ListenAndServe()
+	go srv.ListenAndServe()
 
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, os.Kill)
--- core/telegram.go
+++ core/telegram.go
@@ -1,9 +1,8 @@
 package core
 
-import "fmt"
-
 func (b *tgbot) SetWebHook() error {
 	resp, err := b.client.Get(b.apiUrl + "setWebhook?url=" + b.hookSuffix + b.hookPath)
+	b.Log("Set webhook.", 0)
 	if err != nil {
 		return err
 	}
@@ -13,7 +12,7 @@ func (b *tgbot) SetWebHook() error {
 
 func (b *tgbot) CancelWebHook() error {
 	resp, err := b.client.Get(b.apiUrl + "deleteWebhook")
-	fmt.Println(b.apiUrl + "deleteWebhook")
+	b.Log("Delete webhook.", 0)
 	if err != nil {
 		return err
 	}
