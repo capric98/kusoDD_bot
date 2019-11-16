efbd268852797a3a1ba558922c5c5f9854ccf33d
HZH <capric98@outlook.com>
2019-11-17 05:42:23 +0800

    test

--- plugins/commands/help.go
+++ plugins/commands/help.go
@@ -5,14 +5,14 @@ import (
 )
 
 var helpText = `*kusoDD_bot v0.1.0*
-by *[capric98](https://github.com/capric98)*
+by [capric98](https://github.com/capric98)
 
 /info bot信息
 /help 输出当前内容
 /getsticker 获取一张sticker`
 
 func printHelp(msg Message, bot Tgbot) error {
-	k := []string{"chat_id", "parse_mode", "text"}
-	v := []string{msg.GetChatIDStr(), "Markdown", helpText}
+	k := []string{"chat_id", "parse_mode", "text", "disable_web_page_preview"}
+	v := []string{msg.GetChatIDStr(), "Markdown", helpText, "true"}
 	return bot.SendMessage(k, v)
 }
