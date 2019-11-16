3b12b0c745a83a50749d8e5e61614496c01f391f
HZH <capric98@outlook.com>
2019-11-17 06:01:51 +0800

    refine

--- core/telegram.go
+++ core/telegram.go
@@ -19,7 +19,10 @@ type Tgbot interface {
 	SetWebHook() error
 	CancelWebHook() error
 	SendChatAction([]string, []string) error
+	//action: typing, upload_photo, record_video, upload_video, record_audio, upload_audio,
+	// upload_document, find_location, record_video_note, upload_video_note
 	SendMessage([]string, []string) error
+	//chat_id, text, parse_mode, disable_web_page_preview, disable_notification, reply_to_message_id, reply_markup
 }
 
 type tgresp struct {
@@ -38,7 +41,7 @@ var (
 
 func (b *tgbot) Init() {
 	prefix := "https://api.telegram.org/bot" + b.token + "/"
-	apiUrl["SetWebHook"] = prefix + "setWebhook?url=" + b.hookSuffix + b.hookPath
+	apiUrl["SetWebHook"] = prefix + "setWebhook"
 	apiUrl["CancelWebHook"] = prefix + "deleteWebhook"
 	apiUrl["SendChatAction"] = prefix + "sendChatAction"
 	apiUrl["SendMessage"] = prefix + "sendmessage"
@@ -71,65 +74,50 @@ func NewMultipart(api string, k []string, v []string, ftype string, filename str
 	return
 }
 
-func check(resp *http.Response) (result tgresp) {
-	_ = json.NewDecoder(resp.Body).Decode(&result)
-	resp.Body.Close()
-	return result
-}
-
-func (b *tgbot) SetWebHook() error {
-	resp, err := b.client.Get(apiUrl["SetWebHook"])
-	b.Log("Set webhook.", 0)
-	if err != nil {
-		return err
-	}
-
-	if result := check(resp); result.Ok {
-		return nil
-	} else {
-		return errors.New("telegram: " + result.Description)
-	}
-}
-
-func (b *tgbot) CancelWebHook() error {
-	resp, err := b.client.Get(apiUrl["CancelWebHook"])
-	b.Log("Delete webhook.", 0)
-	if err != nil {
-		return err
-	}
-
-	if result := check(resp); result.Ok {
-		return nil
-	} else {
-		return errors.New("telegram: " + result.Description)
-	}
-}
-
-func (b *tgbot) simpleDo(fname string, k []string, v []string) error {
+func (b *tgbot) call(fname string, k []string, v []string, filetype string, filename string, data []byte) (*tgresp, error) {
 	if len(k) != len(v) {
-		return ErrKVnotFit
+		return nil, ErrKVnotFit
 	}
 	req, ack := NewMultipart(apiUrl[fname], k, v, "", "", nil)
 	defer ack()
 
 	resp, e := b.client.Do(req)
 	if e != nil {
-		return e
+		return nil, e
 	}
 
 	if result := check(resp); result.Ok {
-		return nil
+		return &result, nil
 	} else {
-		return errors.New("telegram: " + result.Description)
+		return nil, errors.New("telegram: " + result.Description)
 	}
 }
 
+func (b *tgbot) simpleCall(fname string, k []string, v []string) (e error) {
+	_, e = b.call(fname, k, v, "", "", nil)
+	return
+}
+
+func (b *tgbot) SetWebHook() error {
+	return b.simpleCall("SetWebHook", []string{"url"}, []string{b.hookSuffix + b.hookPath})
+}
+
+func (b *tgbot) CancelWebHook() (e error) {
+	return b.simpleCall("CancelWebHook", nil, nil)
+}
+
 func (b *tgbot) SendChatAction(k []string, v []string) error {
-	return b.simpleDo("SendChatAction", k, v)
+	return b.simpleCall("SendChatAction", k, v)
 }
 
 func (b *tgbot) SendMessage(k []string, v []string) error {
-	return b.simpleDo("SendMessage", k, v)
+	return b.simpleCall("SendMessage", k, v)
+}
+
+func check(resp *http.Response) (result tgresp) {
+	_ = json.NewDecoder(resp.Body).Decode(&result)
+	resp.Body.Close()
+	return result
 }
 
 func toStr(n int64) string {
