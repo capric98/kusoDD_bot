58ef8cf82dcbc3c2bcb338d9029c7c8f6fc6ba23
HZH <capric98@outlook.com>
2019-11-17 13:31:29 +0800

    need more info

--- core/handle.go
+++ core/handle.go
@@ -27,8 +27,11 @@ func (b *tgbot) Handle(r *http.Request) {
 
 	if e := json.NewDecoder(r.Body).Decode(pmsg); e != nil {
 		b.Log(e, 1)
+	} else {
+		b.Log(pmsg, 0)
+		for _, p := range b.plugins {
+			go p.Handle(pmsg, b)
+		}
 	}
-	for _, p := range b.plugins {
-		go p.Handle(pmsg, b)
-	}
+
 }
