0bdcb99e153ce47ca9e9a428be54a4d0bf6217af
HZH <capric98@outlook.com>
2020-03-15 03:30:09 +0800

    notes about TGS format

---
+++ plugins/getsticker/tgs.go
@@ -0,0 +1,6 @@
+package getsticker
+
+// pip(3) install tgs cairosvg numpy fonttools pillow scipy opencv-python
+// Windows: https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer
+
+// /usr/bin/env python3 tgsconvert.py --input-format lottie --output-format video --sanitize sticker.tgs sticker.avi
